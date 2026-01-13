856c08dc83ba9bb8089676aeec3f886b68331a84
layashin <layash.humagai@intuji.com>
2026-01-13 18:41:56 +0000

    add get tickets route

--- backend/src/routes/ticket.routes.ts
+++ backend/src/routes/ticket.routes.ts
@@ -1,3 +1,6 @@
 import { Router } from "express";
+import { TicketController } from "../controllers/ticket.controller";
 
 export const ticketRouter = Router();
+
+ticketRouter.get("/tickets", TicketController.getTickets);
